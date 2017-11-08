f552fb56b539d2e54963c8489381d7569c3fe3c8
justkiddinglesha <justkiddinglesha@gmail.com>
2017-11-08 23:48:04 +0300

    чистка папок статики

--- frontend/static/dev/server/webpack.js
+++
@@ -1,8 +0,0 @@
-var env = require('../../../../common/env/env.json');
-
-var config = require('../shared/env/' + env.env).webpack;
-
-import webpackDevConfig from '../shared/webpack.dev.js';
-import StartWebpack from 'napishem-frontend-utils/modules/WebpackLocalStart';
-
-StartWebpack("Partner account", config, webpackDevConfig(config), config.port);
